027ef6480b84dc8a052b58b768541f86df79c132
DixonSaavedra <56615526+DixonSaavedra@users.noreply.github.com>
2024-07-24 17:43:06 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Aquí podrías agregar lógica JavaScript para interactuar con la página
+    console.log('La página ha cargado completamente');
+});
